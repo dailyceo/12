3d7177ec3f612686be747207c5b247b8bf4fe5c3
dailyceo <127726398+dailyceo@users.noreply.github.com>
2023-04-10 13:59:03 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -280,30 +280,13 @@ function App() {
                 color: "var(--primary)",
               }}
             >
-              Mint April 10, 12:00 UTC
+              Public Mint Open
             </s.TextDescription>
 
 
 
 
 
-            <s.TextDescription
-              style={{
-                textAlign: "center",
-                fontSize: 40,
-                //fontWeight: "bold",
-                color: "var(--primary)",
-              }}
-            >
-              {truncate(
-                new Date(timeRemaining).toLocaleString("en-GB", {
-                  hour: "numeric",
-                  minute: "numeric",
-                  second: "numeric",
-                }),
-                10
-              )}
-            </s.TextDescription>
 
 
 
